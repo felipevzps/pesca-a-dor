56781f0c5a3711e30aefe58935d9684023a89799
felipevzps <felipe.vzps@gmail.com>
2023-04-01 22:20:16 -0300

    minor changes in filenames

--- locateOnScreen.py
+++ locateOnScreen.py
@@ -17,9 +17,9 @@ MINIGAME_REGION_FISH = (236,178,14,14)
 pyautogui.PAUSE = 0.01 
 
 while True:
-    bubble = pyautogui.locateOnScreen('bubble_1024_vermilion.PNG', confidence=0.7)#, region=FISHING_POSITIONS+IMG_BUBBLE_SIZE)
-    bar = pyautogui.locateOnScreen('bar_1024.PNG', confidence=0.7)#, region=MINIGAME_REGION_BAR)
-    fish = pyautogui.locateOnScreen('fish_1024.PNG', confidence=0.7, grayscale=True)#, region=MINIGAME_REGION_FISH)
+    bubble = pyautogui.locateOnScreen(bubble_igm, confidence=0.7)#, region=FISHING_POSITIONS+IMG_BUBBLE_SIZE)
+    bar = pyautogui.locateOnScreen(bar_img, confidence=0.7)#, region=MINIGAME_REGION_BAR)
+    fish = pyautogui.locateOnScreen(fish_img, confidence=0.7, grayscale=True)#, region=MINIGAME_REGION_FISH)
     print("bubble",bubble)
     print("bar",bar)
     print("fish",fish)
