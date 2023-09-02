c195cceed388d3dbd9f57d5d0bafe6af8581d4c4
Felipe Vaz Peres <felipe.vzps@gmail.com>
2023-09-02 19:43:33 -0300

    Update locateOnScreen.py documentation

--- locateOnScreen.py
+++ locateOnScreen.py
@@ -3,58 +3,51 @@ import pyautogui
 import win32gui
 from time import sleep
 
-#IMG PATH
 RESOLUTION='1024x768'
 
+# IMG PATH
 bubble_img='images/{}/bubble.PNG'.format(RESOLUTION)
-bar_img='bar_1024x768.PNG'
-fish_img='fish_1024x768_binarizada.PNG'
-hungry_img='images/{}/hungry.PNG'.format(RESOLUTION)
-krabby_img='krabby_1024x768.PNG'
-tentacool_img='images/{}/tentacool1.PNG'.format(RESOLUTION)
 hook_img='images/{}/hook.PNG'.format(RESOLUTION)
+bar_img='images/{}/bar.PNG'.format(RESOLUTION)
+fish_img='images/{}/fish_bin.PNG'.format(RESOLUTION)
+hungry_img='images/{}/hungry.PNG'.format(RESOLUTION)
+shiny_img='images/{}/shiny.PNG'.format(RESOLUTION)
+krabby_img='images/{}/krabby.PNG'.format(RESOLUTION)
+tentacool_img='images/{}/tentacool.PNG'.format(RESOLUTION)
 feebas_img='images/{}/feebas.PNG'.format(RESOLUTION)
 giant_karp_img='images/{}/giant_karp.PNG'.format(RESOLUTION)
-shiny_giant_karp='images/{}/shiny_giant_karp.PNG'.format(RESOLUTION)
+shiny_giant_karp_img='images/{}/shiny_giant_karp.PNG'.format(RESOLUTION)
 
+pyautogui.PAUSE = 0.01 # pyautogui delay default = 0.1
 
-#POSITIONS
-FISHING_POSITIONS = (514, 382) #viridian
-IMG_BUBBLE_SIZE = (25,28)
-MINIGAME_REGION_BAR = (189,555,15,38)
-MINIGAME_REGION_FISH = (236,178,14,14)
-
-#pyautogui.PAUSE = 0.1 # default
-pyautogui.PAUSE = 0.01 
-
-#pxg = win32gui.FindWindow(None, "PokeXGames")
-#x, y, largura, altura = win32gui.GetWindowRect(pxg)
-
-# Ajustar as coordenadas para a posição da janela na tela
-#x, y = pyautogui.position()[0] - x, pyautogui.position()[1] - y
-
-#PXG Client
-#<Win32Window left="-8", top="-8", width="1040", height="744", title="PokeXGames">
-#print()
-
+# Function to locate the (left, top) position of each image
 while True:
-    bubble = pyautogui.locateOnScreen(bubble_img, confidence=0.7)#, region=FISHING_POSITIONS+IMG_BUBBLE_SIZE)
-    #bubble = pyautogui.locateOnScreen(bubble_img, confidence=0.7, region=(x, y, largura, altura))   
-    #bar = pyautogui.locateOnScreen(bar_img, confidence=0.7)
+    bubble = pyautogui.locateOnScreen(bubble_img, confidence=0.7)
     hook = pyautogui.locateOnScreen(hook_img, confidence=0.5)
-
-    #fish = pyautogui.locateOnScreen(fish_img, confidence=0.7, grayscale=True)
+    bar = pyautogui.locateOnScreen(bar_img, confidence=0.7)
+    fish = pyautogui.locateOnScreen(fish_img, confidence=0.7, grayscale=True)
     hungry = pyautogui.locateOnScreen(hungry_img, confidence=0.91, region=(982,236,17,20))
-    #krabby = pyautogui.locateOnScreen(krabby_img, confidence=0.7)
-    tentacool = pyautogui.locateOnScreen(tentacool_img, confidence=0.91)
+    krabby = pyautogui.locateOnScreen(krabby_img, confidence=0.7)
+    tentacool = pyautogui.locateOnScreen(tentacool_img, confidence=0.88)
     feebas = pyautogui.locateOnScreen(feebas_img, confidence=0.8)
     giant_karp = pyautogui.locateOnScreen(giant_karp_img, confidence=0.90)
+    shiny_giant_karp = pyautogui.locateOnScreen(shiny_giant_karp_img, confidence=0.95)
+
+    # Mouse position
     print(pyautogui.position())
+    
+    # Bubbles and hook positions
     print("bubble",bubble)
-    print("giant_karp",giant_karp)
-    #print("bar",bar)
-    #print("fish",fish)
     print("hook",hook)
+
+    # Minigame features positions
+    print("bar",bar)
+    print("fish",fish)
+
+    # Hungry position (the icon must be RED if the pokemon is hungry)
     print("hungry",hungry)
-    #print("krabby",krabby)
+
+    # Dead pokemon positions
+    print("krabby",krabby)
     print("tentacool",tentacool)
+    print("giant_karp",giant_karp)
