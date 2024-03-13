07e4ce5bba754d0955ef7119296247a3ffd38fc6
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-03-12 21:47:15 -0300

    garbage, just run today

--- pesca_a_dor/pesca-a-dor.py
+++ pesca_a_dor/pesca-a-dor.py
@@ -3,28 +3,38 @@ import keyboard
 import config
 import threading
 import functions
+import json
 
 print(">>> Press 'P' to start <<<")
 keyboard.wait('p')
 
 functions.log_message("Started fishing")
 
+with open("pesca_a_dor/infos.json", "r") as file:
+    config_json = json.load(file)
+
 while config.counter < config.minigame_repeats: 
-    
+
     fishing_position = functions.set_fishing_rod()
 
-    functions.start_and_join_thread(functions.threadKillShiny,functions.kill_shiny,(config.KILL_POKEMON_LIST, config.USE_THREAD_KILL_SHINY))
-    functions.start_and_join_thread(functions.threadSomeActions,functions.some_actions,(config.USE_THREAD_KILL_SHINY,))
-    
-    if functions.constant_search_dragon() and config.USE_THREAD_BALL_DRAGON:
+    functions.start_and_join_thread(functions.threadKillShiny,functions.kill_shiny,(config.KILL_POKEMON_LIST, config_json["USE_THREAD_KILL_SHINY"]))
+    functions.start_and_join_thread(functions.threadSomeActions,functions.some_actions,(config_json["USE_THREAD_KILL_SHINY"],))
+
+    if functions.constant_search_dragon() and config_json["USE_THREAD_BALL_DRAGON"]:
+
         functions.threadSearchDragon = threading.Thread(target=functions.ball_dragon)
         functions.threadSearchDragon.start()
+        
+    # To catch normal dratini / dragonair
+    #if config_json["USE_THREAD_BALL_DRAGON"]:
+        #unctions.threadSearchNormal = threading.Thread(target=functions.ball_normal)
+        #functions.threadSearchNormal.start()
 
     functions.wait_bubble(fishing_position)
     config.counter = functions.minigame(config.counter)
 
-    if functions.find_elixir() and config.FISH_MAGIKARP:
-        config.USE_THREAD_KILL_SHINY = functions.apply_elixir_mode(config.USE_THREAD_KILL_SHINY)
+    if functions.find_elixir() and config_json["FISH_MAGIKARP"]:
+        config_json["USE_THREAD_KILL_SHINY"] = functions.apply_elixir_mode(config_json["USE_THREAD_KILL_SHINY"])
 
     functions.logout(config.counter)
 
